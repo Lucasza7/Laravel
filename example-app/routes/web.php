de81df6b4a29d504d835e7efa58a597b2f006664
lucas <l.zegger0@gmail.com>
2023-11-02 09:41:34 +0100

    alle opdrachten

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PlanetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,30 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::get('/', function () {
-    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    return view('about');
 });
 
-Route::get('/', function (){
-    $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
-return view ('welcome', ['planets' => $planets]);
+Route::get('/EersteRoute', function () {
+    return view('pages/EersteRoute');
 });
+
+
+Route::get('/planets1', function () {
+    return view("pages/Views");
+});
+
+Route::get('/planets2', function () {
+    return view("pages/Requests");
+});
+
+
+Route::get('/planets', [PlanetController::class, 'index']);
+
+Route::get('/planets/{planet}', [PlanetController::class, 'show']);
